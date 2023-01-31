45fea76bdea1f1bfbbaab08e028b13af6c985a9d
IanBruch <23bruchia@muskegonorwayschools.org>
2023-01-30 19:15:12 -0600

    Added compressor, PDH, and pneumatic hub

--- 2023-code/src/main/java/frc/robot/RobotContainer.java
+++ 2023-code/src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,10 @@ package frc.robot;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -27,6 +31,11 @@ public class RobotContainer {
   // Set up controller with CommandXboxController
   private final CommandXboxController driverController;
 
+  private static Compressor compressor;
+
+  public final PowerDistribution PDH;
+  public final PneumaticHub pneumaticHub;
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     driveSubsystem = new DriveSubsystem();
@@ -44,6 +53,18 @@ public class RobotContainer {
         driverController.getRightX() * .75, 
         driverController.getLeftTriggerAxis() * DriveConstants.DRIVE_NERF_JOYSTICK_MULTIPLIER,
         driverController.getRightTriggerAxis() * DriveConstants.DRIVE_NERF_JOYSTICK_MULTIPLIER), driveSubsystem));
+
+    //configures the compressor
+    compressor = new Compressor(PneumaticsModuleType.REVPH);
+    compressor.enableAnalog(95, 120);
+
+    //configures the PDH
+    PDH = new PowerDistribution();
+    PDH.clearStickyFaults();
+
+    //configures the Pneumatic Hub
+    pneumaticHub = new PneumaticHub();
+    pneumaticHub.clearStickyFaults();
 
     // Configure the trigger bindings
     configureBindings();

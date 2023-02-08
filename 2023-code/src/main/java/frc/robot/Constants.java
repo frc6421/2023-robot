803bef1ac095411c51320e8b27e328f09a02ab82
EvanRoegner <roegnere@gmail.com>
2023-02-07 18:06:43 -0600

    Updated offsets

--- 2023-code/src/main/java/frc/robot/Constants.java
+++ 2023-code/src/main/java/frc/robot/Constants.java
@@ -82,22 +82,22 @@ public final class Constants {
     public static final int FRONT_LEFT_MODULE_DRIVE_CAN_ID = 12;
     public static final int FRONT_LEFT_MODULE_STEER_CAN_ID = 13;
     public static final int FRONT_LEFT_MODULE_ENCODER_CAN_ID = 13;
-    public static final double FRONT_LEFT_MODULE_ANGLE_OFFSET = -171.6;
+    public static final double FRONT_LEFT_MODULE_ANGLE_OFFSET = -62.05;
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_CAN_ID = 10;
     public static final int FRONT_RIGHT_MODULE_STEER_CAN_ID = 11;
     public static final int FRONT_RIGHT_MODULE_ENCODER_CAN_ID = 11;
-    public static final double FRONT_RIGHT_MODULE_ANGLE_OFFSET = -137.0;
+    public static final double FRONT_RIGHT_MODULE_ANGLE_OFFSET = -350.51;
 
     public static final int BACK_LEFT_MODULE_DRIVE_CAN_ID = 16;
     public static final int BACK_LEFT_MODULE_STEER_CAN_ID = 17;
     public static final int BACK_LEFT_MODULE_ENCODER_CAN_ID = 17;
-    public static final double BACK_LEFT_MODULE_ANGLE_OFFSET = -192.5;
+    public static final double BACK_LEFT_MODULE_ANGLE_OFFSET = -108.02;
 
     public static final int BACK_RIGHT_MODULE_DRIVE_CAN_ID = 14;
     public static final int BACK_RIGHT_MODULE_STEER_CAN_ID = 15;
     public static final int BACK_RIGHT_MODULE_ENCODER_CAN_ID = 15;
-    public static final double BACK_RIGHT_MODULE_ANGLE_OFFSET = -352.5;
+    public static final double BACK_RIGHT_MODULE_ANGLE_OFFSET = -302.43;
 
     public static final double MODULE_DRIVE_P = 0.208; 
     public static final double MODULE_DRIVE_I = 0; 

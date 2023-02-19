a03997dfcb8c89c4fa43e6ddf9381bd53777a95a
KierstinS <24schlevenskyki@muskegonorwayschools.org>
2023-02-18 18:05:43 -0600

    Flipped one piece charge path

--- 2023-code/src/main/java/frc/robot/commands/autonomousCommands/FlippedOnePieceChargeCommand.java
+++ 2023-code/src/main/java/frc/robot/commands/autonomousCommands/FlippedOnePieceChargeCommand.java
@@ -25,11 +25,10 @@ import frc.robot.subsystems.DriveSubsystem;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class OnePieceChargeCommand extends SequentialCommandGroup {
+public class FlippedOnePieceChargeCommand extends SequentialCommandGroup {
   private DriveSubsystem driveSubsystem;
-
-  /** Creates a new OnePieceChargeCommand. */
-  public OnePieceChargeCommand(DriveSubsystem drive) {
+  /** Creates a new FlippedOnePieceChargeCommand. */
+  public FlippedOnePieceChargeCommand(DriveSubsystem drive) {
     driveSubsystem = drive;
     addRequirements(driveSubsystem);
 
@@ -44,12 +43,12 @@ public class OnePieceChargeCommand extends SequentialCommandGroup {
         .setKinematics(driveSubsystem.swerveKinematics)
         .setReversed(true);
 
-    // Starts in line with the right cone node in co-op grid
+    // Starts in line with the left cone node in co-op grid
     Trajectory chargeStationTrajectory = TrajectoryGenerator.generateTrajectory(List.of(
-        new Pose2d(TrajectoryConstants.SECOND_COOPERTITION_CONE_NODE, new Rotation2d(0)),
-        new Pose2d(TrajectoryConstants.FAR_EDGE_OF_COMMUNITY, new Rotation2d(0)),
-        new Pose2d(TrajectoryConstants.MID_POINT_OF_PIECES_AND_CHARGE_STATION, new Rotation2d(0)),
-        new Pose2d(TrajectoryConstants.CENTER_OF_CHARGE_STATION, new Rotation2d(0))), forwardConfig);
+        new Pose2d(TrajectoryConstants.FLIPPED_SECOND_COOPERTITION_CONE_NODE, new Rotation2d(0)),
+        new Pose2d(TrajectoryConstants.FLIPPED_FAR_EDGE_OF_COMMUNITY, new Rotation2d(0)),
+        new Pose2d(TrajectoryConstants.FLIPPED_MID_POINT_OF_PIECES_AND_CHARGE_STATION, new Rotation2d(0)),
+        new Pose2d(TrajectoryConstants.FLIPPED_CENTER_OF_CHARGE_STATION, new Rotation2d(0))), forwardConfig);
 
     var thetaController = new ProfiledPIDController(
         AutoConstants.THETA_P, AutoConstants.THETA_I, AutoConstants.THETA_D,
